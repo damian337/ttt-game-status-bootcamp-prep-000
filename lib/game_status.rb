8d715c1fc21424bae86406a2be31557e89f6e03a
Damian Guillory <guillorywdamian@gmail.com>
2017-10-24 02:32:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 
 # checks to see if there is a winner
 def won?(board)
-    
+
     WIN_COMBINATIONS.each do |win_combination|
         win_index_1 = win_combination[0]
         win_index_2 = win_combination[1]
